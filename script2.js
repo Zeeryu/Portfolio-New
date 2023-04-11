a39b798879bef5c111bc9af6fc96bd57677ba597
Esmin Pasic <87947564+Zeeryu@users.noreply.github.com>
2023-04-11 14:34:50 -0700

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,28 @@
+var menuBtn = document.getElementById("menuBtn")
+        var sideNav = document.getElementById("sideNav")
+        var menu = document.getElementById("menu")
+        sideNav.style.right == "-250px";
+        menuBtn.onclick = function() {
+            if (sideNav.style.right == "-250px") {
+                sideNav.style.right = "0";
+                menu.src = "https://i.postimg.cc/cJRss6PP/close.png";
+            } else {
+                sideNav.style.right = "-250px";
+                menu.src = "https://i.postimg.cc/j5RRCtb2/menu.png";
+            }
+}
+
+var btnMenu = document.getElementsByClassName("btn-menu");
+var body = document.body
+for (var i = 0; i < btnMenu.length; i++) {
+    btnMenu[i].addEventListener('click', function () {
+        body.classList.toggle('menu-open');
+    });
+}
+
+
+$(document).ready(function() {
+    $(".dropdown-button").click(function() {
+      $(".dropdown-content").toggleClass("show");
+     });
+});
